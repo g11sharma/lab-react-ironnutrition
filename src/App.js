7bcc179c36f9d95f2f14e75c36a12eaa40fb615b
gaurav sharma <g11.sharma@gmail.com>
2022-06-23 23:31:14 +0200

    itIteration 5-6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
+import { Row, Divider, Button, Card } from 'antd';
+import 'antd/dist/antd.css';
 
 import foodlist from './foods.json';
 import FoodBox from './componnents/FoodBox';
 import AddFoodForm from './componnents/AddFoodForm';
+import SearchForm from './componnents/SearchForm';
 
 import './App.css';
 
@@ -17,21 +20,32 @@ function App() {
     copy.push(newfood);
     setFoods(newfood);
   }
+  function deleteFood(food) {
+    setFoods(foods.filter((x) => x !== food));
+    console.log(deleteFood);
+  }
 
   const addNewFood = (newFood) => {
     //add it to the list of Food
     setFoods([...foods, newFood]);
   };
 
+  function searchFood() {
+    const foodSearch = foodData.filter((food) => food.name.match(query));
+    setFoods(foodSearch);
+  }
+
   return (
     <div>
-      <h2>FoodList</h2>
+      <AddFoodForm addNewFood={addNewFood} />
+      <SearchForm searchFood={searchFood} />
+      <h2>Food List</h2>
+      <Row style={{ width: '100%', justifyContent: 'center' }}></Row>
       <div>
         {foods.map((foods) => (
-          <FoodBox {...foods} />
+          <FoodBox {...foods} deleteFood={deleteFood} />
         ))}
       </div>
-      <AddFoodForm addNewFood={addNewFood} />
     </div>
   );
 }
